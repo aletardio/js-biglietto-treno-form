ae37c644eafe282596c1ce35bbd8feb368b89065
aletardio <ale.tardio99@gmail.com>
2023-11-21 15:38:38 +0100

    added script with button addeventlistener

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+const button = document.querySelector('.btn-generate');
+
+button.addEventListener('click', function(){
+    let name = document.getElementById('name').value;
+    let kilometers = document.getElementById('kilometers').value;
+    let age = document.getElementById('age').value;
+
+    let price_start = 0.21 * kilometers;
+    let price_final = '';
+
+})
