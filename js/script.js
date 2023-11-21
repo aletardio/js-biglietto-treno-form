08159008b9607cd61835a8aa93092453252116ce
aletardio <ale.tardio99@gmail.com>
2023-11-21 15:43:47 +0100

    added script with if ì, else if and else

--- js/script.js
+++ js/script.js
@@ -9,4 +9,18 @@ button.addEventListener('click', function(){
     let price_start = 0.21 * kilometers;
     let price_final = '';
 
+    if (age == 'Min') {
+        // Istruzioni per il calcolo del prezzo scontato del 20%
+        let sale = price_start * 20 / 100;
+        price_final = price_start - sale;
+    }
+    else if (age == 'Max') {
+        // Istruzioni per il calcolo del prezzo scontato del 40%
+        let sale = price_start * 40 / 100;
+        price_final = price_start - sale;
+    }
+    else {
+        // Istruzioni per il calcolo del prezzo non scontato
+        price_final = price_start;
+    }
 })
